92057d3cd973937c379f0e5edac5c8044ad8f63c
Nick Tyranov <nicktyranov@gmail.com>
2024-12-18 22:02:19 -0700

    lesson 23/25

--- Tasks/22. Реализуем статический сервер на NodeJS/22.task-5/server.js
+++ Tasks/22. Реализуем статический сервер на NodeJS/22.task-5/server.js
@@ -4,65 +4,66 @@ import path from 'path';
 
 // Сделайте сайт о вашем городе. Пусть сайт состоит из 6-ти HTML страниц. К этим страницам должен быть подключен общий CSS файл, общий JavaScript файл, добавлены картинки. На каждой странице должна быть менюшка, с помощью которой можно будет перемещаться по страницам сайта.
 
-http.createServer(async (request, response) => {
-	const rootDir = path.join(process.cwd());
-	
-	if (request.url == '/favicon.ico') {
-		response.writeHead(200, { 'Content-Type': 'image/x-icon' });
-		response.write(await fs.promises.readFile(path.join(rootDir, 'src', 'favicon.ico')));
-		response.end();
-		return;
+// №6⊗ndPmSvSt
+// Уберите из ваших адресов расширения HTML файлов.
 
-	}
+http.createServer(async (request, response) => {
+	const rootDir = path.join(process.cwd(), 'src'); // Корневая директория для файлов
+
 	let statusCode;
 	let data;
-		
-	let filePath = path.join(rootDir, 'src', request.url);
+
+	let filePath = path.join(rootDir, request.url);
 	console.log('Request path:', filePath);
 
 	try {
-		const stats = await fs.promises.stat(filePath);
-		if (stats.isDirectory()) {
-			filePath += '/index.html';
+		let stats;
+		try {
+			stats = await fs.promises.stat(filePath);
+		} catch {
+			filePath += '.html';
+			stats = await fs.promises.stat(filePath);
 		}
+
+		if (stats.isDirectory()) {
+			filePath = path.join(filePath, 'index.html');
+		}
+
 		data = await fs.promises.readFile(filePath);
-		statusCode = 200;			
+		statusCode = 200;
 	} catch (e) {
 		console.error('Ошибка при обработке запроса:', e);
-		filePath = path.join(rootDir, 'src', 'pages', '404.html');
-		statusCode = 404;
-		data = await fs.promises.readFile(filePath);
+
+		filePath = path.join(rootDir, 'pages', '404.html');
+		try {
+			data = await fs.promises.readFile(filePath);
+			statusCode = 404;
+		} catch {
+			data = '404 Not Found';
+			statusCode = 404;
+		}
 	}
 
-	response.writeHead(statusCode, { 'Content-Type': getMimeType(filePath) });
-	response.write(data);
-	response.end();
-}
-).listen(3001);
+	const contentType = getMimeType(path.extname(filePath).slice(1));
+	response.writeHead(statusCode, { 'Content-Type': contentType });
+	response.end(data);
+}).listen(3000, () => {
+	console.log('Server is running at http://localhost:3000');
+});
 
-function getMimeType(path) {
-	let mimes = {
+function getMimeType(ext) {
+	const mimes = {
 		html: 'text/html',
-		jpeg: 'image/jpeg',
-		jpg:  'image/jpeg',
+		css: 'text/css',
+		js: 'application/javascript',
 		png: 'image/png',
+		jpg: 'image/jpeg',
+		jpeg: 'image/jpeg',
+		ico: 'image/x-icon',
 		avif: 'image/avif',
 		webp: 'image/webp',
-		svg:  'image/svg+xml',
-		json: 'application/json',
-		js:   'text/javascript',
-		css:  'text/css',
-		ico:  'image/x-icon'
+		svg: 'image/svg+xml',
+		json: 'application/json'
 	};
-	
-	let exts = Object.keys(mimes);
-	let extReg = new RegExp('\\.(' + exts.join('|') + ')$');
-	
-	let ext = path.match(extReg)[1];
-	
-	if (ext) {
-		return mimes[ext];
-	} else {
-		return 'text/plain';
-	}
+	return mimes[ext] || 'text/plain';
 }
